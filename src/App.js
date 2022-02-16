1fc6180ad8e8fea7852d30db5f18a314934d51be
Griffin Alcorn <griffin.alcorn@gmail.com>
2022-02-16 15:13:40 -0500

    img slider implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import SliderData from './components/SliderData';
 import ImageSlider from './components/ImageSlider';
 
 function App() {
   return (
     <div className="App">
       <h3>Griffin Alcorn</h3>
-      <ImageSlider />
+      <ImageSlider slides={SliderData}/>
     </div>
   );
 }
